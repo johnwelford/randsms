b757dede75239655f882bbffb343745edfded98f
HackyManGrunthos <sam_hole@hotmail.com>
2013-10-26 18:48:01 +0100

    Added file output on form submit

--- TestAppRecv/welcome.php
+++ TestAppRecv/welcome.php
@@ -2,7 +2,11 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-Updated!
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Opening file<br>
+$fid = fopen("subsDatabase.csv", "a");
+File open <?php echo $fid; ?><br>
+$rval = bool fclose($fid);
+File closed result <?php echo $rval; ?>
 </body>
 </html>
