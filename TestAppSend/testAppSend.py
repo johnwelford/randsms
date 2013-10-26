2948e9ed2e39a9aca3957f7b65612fc2d52ab7b3
HackyManGrunthos <sam_hole@hotmail.com>
2013-10-27 00:30:31 +0100

    Updated cgi send example to use a hacked up lxml\!

--- TestAppSend/testAppSend.py
+++ TestAppSend/testAppSend.py
@@ -1,10 +1,22 @@
-from clockwork import clockwork
- 
+#!/usr/bin/env python
+import sys
+sys.path.append('./lxml-3.2.3/src')
+import clockwork
+
+#print "Content-Type: text/html"
+#print
+#print """\
+#<html>
+#<body>
+#<h2>Hello World!</h2>
+#</body>
+#</html>
+#"""
 api = clockwork.API('f28dbd49244e960260e9a5d40de3411fb635ac5d')
  
 message = clockwork.SMS(
     to = '447814963513',
-    message = 'Imagine a different random number.',
+    message = 'What number am I think of.',
     from_name = 'RandSMS')
 
  
