1bba2ccca0f587dd1a187aeb8b5ebf1c0dfb076d
john <john.welford@gmail.com>
2013-10-27 00:48:21 +0100

    betterised python

--- TestAppRecv/welcome.py
+++ TestAppRecv/welcome.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
+
+import cgi
+form = cgi.FieldStorage()
 print "Content-Type: text/html"
+
+
 print
 print """\
 <html>
 <body>
 <h2>Hello World!</h2>
+"""
+print "The user entered %s" % form.getvalue("name")
+print """\
 </body>
 </html>
 """
+
+
+
