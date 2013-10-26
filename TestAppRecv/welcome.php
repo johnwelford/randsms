899bbdb543584ce62c737aef0d9172cf8333d68a
HackyManGrunthos <sam_hole@hotmail.com>
2013-10-26 19:16:42 +0100

    fixed php syntax

--- TestAppRecv/welcome.php
+++ TestAppRecv/welcome.php
@@ -5,8 +5,8 @@ Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
 Opening file<br>
 <?$fid = fopen("subsDatabase.csv", "a"); ?>
-File open <?php echo $fid; ?><br>
+<!-- File open <?php echo $fid; ?><br> -->
 <?$rval = bool fclose($fid); ?>
-File closed result <?php echo $rval; ?>
+<!-- File closed result <?php echo $rval; ?> -->
 </body>
 </html>
