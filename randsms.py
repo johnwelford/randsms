63c96767efd800b0487053401e8a6dc36beff6f7
HackyManGrunthos <sam_hole@hotmail.com>
2013-10-27 05:30:55 +0000

    Removed the stupid server error condition

--- randsms.py
+++ randsms.py
@@ -45,4 +45,13 @@ message = clockwork.SMS(
 
 response = api.send(message)
 
-print "HTTP/1.1 200 OK"
+print "Content-Type: text/html"
+print
+print """\
+<html>
+<body>
+<h2>Success!</h2>
+</body>
+</html>
+"""
+#print "HTTP/1.1 200 OK"
