e573cfa700426ca05abb5e11da0bc72d20ef7706
john <john.welford@gmail.com>
2013-10-27 02:11:41 +0000

    changed back to hard coded values

--- randsms.py
+++ randsms.py
@@ -27,8 +27,8 @@ print "HTTP/1.1 200 OK"
 
 
 message = clockwork.SMS(
-    to = form.getvalue("from"),
-    message = form.getvalue("content"),
+    to = '447814963513',
+    message = 'Actually it is  BATMAN',
     from_name = 'RandSMS')
 
 response = api.send(message)
