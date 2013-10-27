12bef274dc99a81e281e24aae8c865b94853ba27
john <john.welford@gmail.com>
2013-10-27 02:21:55 +0000

    randomise number returned

--- randsms.py
+++ randsms.py
@@ -14,27 +14,23 @@ import clockwork
 
 api = clockwork.API('f28dbd49244e960260e9a5d40de3411fb635ac5d')
 
-
-# number = int(form.getvalue("number"))
 f = open('MessageRevievedFile.csv', 'a')
 string = "%s, %s\n" %(form.getvalue("from"), form.getvalue("content"))
 f.write(string)
 f.close();
 
-print "HTTP/1.1 200 OK"
-
-#print "The user entered %s" % random.randrange(number)
+number = random.randrange(int(form.getvalue("number")))
 
 
 message = clockwork.SMS(
     to = form.getvalue("from"),
-    message = form.getvalue("content"),
+    message = number,
     from_name = 'RandSMS')
 
 response = api.send(message)
 
 if response.success:
-    print (response.id)
+    print "HTTP/1.1 200 OK"
 else:
-    print (response.error_code)
-    print (response.error_description)
+    print "HTTP/1.1 500 Internal Server Error"
+
