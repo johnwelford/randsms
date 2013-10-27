cb73edec11539e49e5e2a8749edd19a953195a6d
john <john.welford@gmail.com>
2013-10-27 02:47:18 +0000

    manipulate number

--- randsms.py
+++ randsms.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python
+# libraries and input setup
 import cgitb
 cgitb.enable()
-
 import cgi
-import random
 form = cgi.FieldStorage()
-
+import random
 import sys
-sys.path.append('../utils/lxml-3.2.3/src')
-sys.path.append('../utils/clockwork')
-
+#sys.path.append('../utils/lxml-3.2.3/src')
+#sys.path.append('../utils/clockwork')
 import clockwork
-
 api = clockwork.API('f28dbd49244e960260e9a5d40de3411fb635ac5d')
 
+# write to log file
 f = open('MessageRevievedFile.csv', 'a')
 string = "%s, %s\n" %(form.getvalue("from"), form.getvalue("content"))
 f.write(string)
 f.close();
 
-number = random.randrange(int(form.getvalue("number")))
+# randomise
+number = random.randrange(int(float(form.getvalue("number"))))
 
-
+# send response
 message = clockwork.SMS(
     to = form.getvalue("from"),
     message = number,
@@ -29,6 +28,7 @@ message = clockwork.SMS(
 
 response = api.send(message)
 
+# produce response
 if response.success:
     print "HTTP/1.1 200 OK"
 else:
