093684f8e5ae8fb7432c362ecc8696a6e0753cee
HackyManGrunthos <sam_hole@hotmail.com>
2013-10-27 01:59:06 +0100

    Modified the push cgi request to output to file

--- TestAppRecv/welcome.py
+++ TestAppRecv/welcome.py
@@ -3,20 +3,23 @@
 import cgi
 import random
 form = cgi.FieldStorage()
-print "Content-Type: text/html"
+#print "Content-Type: text/html"
 number = int(form.getvalue("number"))
 
-print
-print """\
-<html>
-<body>
-<h2>Hello World!</h2>
-"""
-print "The user entered %s" % random.randrange(number)
-print """\
-</body>
-</html>
-"""
-
-
-
+#print
+#print """\
+#<html>
+#<body>
+#<h2>Hello World!</h2>
+#"""
+#print "The user entered %s" % random.randrange(number)
+f = open('MessageRevievedFile.csv', 'a')
+print >>f "The user entered %s" % random.randrange(number)
+f.close();
+#print """\
+#</body>
+#</html>
+#"""
+#
+#
+#
